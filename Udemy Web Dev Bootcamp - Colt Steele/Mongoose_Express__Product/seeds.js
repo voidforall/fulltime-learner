aa68efcd7d1a1db5113bf8b86937ffb56124935e
Lin Yuan <voidforall@gmail.com>
2022-06-14 02:09:40 +0200

    Added Express with Mongoose

---
+++ Udemy Web Dev Bootcamp - Colt Steele/Mongoose_Express__Product/seeds.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!")
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!")
+        console.log(err)
+    })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Mini Seedless Watermelon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    },
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => {
+        console.log(e)
+    })
